a5f97a6c8d54d41754643916c9e13f1c4b5abab6
mafruhamanal <mafruhamanal1@gmail.com>
2024-06-26 01:48:51 -0400

    tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">Base file!</h1>;
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
 
-export default App;
+export default App
