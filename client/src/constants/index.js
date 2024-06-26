5326943675978214c5ca96fd3dba064e871bde10
mafruhamanal <mafruhamanal1@gmail.com>
2024-06-26 16:14:20 -0400

    Navbar and Constants

---
+++ client/src/constants/index.js
@@ -0,0 +1,74 @@
+import project1 from "../assets/projects/project-1.jpg";
+
+
+export const HERO_CONTENT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut enim ad minim veniam.`;
+
+export const ABOUT_TEXT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut enim ad minim veniam.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Software Developer Intern",
+    company: "University at Buffalo",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+    technologies: ["Javascript", "React Native", "Unity", "C#"],
+  },
+  {
+    year: "2024 - Present",
+    role: "Software Developer Intern",
+    company: "University at Buffalo",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+    technologies: ["Javascript", "React Native", "Unity", "C#"],
+  },
+  {
+    year: "2024 - Present",
+    role: "Software Developer Intern",
+    company: "University at Buffalo",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+    technologies: ["Javascript", "React Native", "Unity", "C#"],
+  },
+  {
+    year: "2024 - Present",
+    role: "Software Developer Intern",
+    company: "University at Buffalo",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+    technologies: ["Javascript", "React Native", "Unity", "C#"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Clay Clutter App",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Clay Clutter App",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Clay Clutter App",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Clay Clutter App",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+];
+
+export const CONTACT = {
+  address: "Buffalo, New York, NY 14215 ",
+  phoneNo: "+1 716 986 2914",
+  email: "mafruhamanal1@gmail.com",
+};
