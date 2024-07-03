db82d34bed96ecef898c4afac51a2e373ca11fa8
mafruhamanal <mafruhamanal1@gmail.com>
2024-07-03 19:51:11 -0400

    framer motion hero component

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,29 +1,57 @@
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/profilepic 1.png";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-24 lg:mb-35 ">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-medium tracking-tight lg:mt-16 lg:text-8xl text-neutral-700">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-medium tracking-tight lg:mt-16 lg:text-8xl text-neutral-700"
+            >
               Hello, I'm Mafruha !
-            </h1>
+            </motion.h1>
 
-            <span className="bg-gradient-to-r font-medium from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r font-medium from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+            >
               CS @ University at Buffalo
-            </span>
-            <p className="my-2 max-w-xl py-6 text-neutral-700">
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 text-neutral-700"
+            >
               {HERO_CONTENT}
               <p className=" max-w-xl py-6 ">
                 Feel free to reach out and say Hi 👋🏼
               </p>
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img src={profilePic} alt="Maf" className="w-3/4" />
+            <motion.img initial={{x:100, opacity:0}}
+            animate={{x:0, opacity:1}}
+            transition={{duration: 1, delay: 1.2}}
+            src={profilePic} alt="Maf" className="w-3/4 py-12" />
           </div>
         </div>
       </div>
