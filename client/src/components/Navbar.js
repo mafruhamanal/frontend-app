60ea19f00da765dfc4be1bdbdf7f97cee00710a3
mafruhamanal <mafruhamanal1@gmail.com>
2024-07-04 05:04:34 -0400

    im sleepy

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,7 +6,7 @@ import { MdOutlineEmail } from "react-icons/md";
 
 const Navbar = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6">
+    <nav className="mb-12 flex items-center justify-between py-2">
       <div className="flex flex-shrink-0 items-center">
       <a  href= "mailto: mafruhamanal1@gmail.com" className=" flex items-center justify-center gap-4 text-2xl text-neutral-800">
       <MdOutlineEmail />
