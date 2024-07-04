bd33f527cd10103d88aeb7e20801092654f184ca
mafruhamanal <mafruhamanal1@gmail.com>
2024-07-04 03:01:55 -0400

    assets

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -8,33 +8,34 @@ export const ABOUT_TEXT = `I love to code (🤯) and enjoy working on little pro
 
 export const EXPERIENCES = [
   {
-    year: "2024 - Present",
-    role: "Software Developer Intern",
-    company: "University at Buffalo",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
-    technologies: ["Javascript", "React Native", "Unity", "C#"],
+    year: "June 2024 - Present",
+    role: "Software Engineer Intern",
+    company: "X Lab",
+    description: `Develop a cross-platform mobile app in React Native to monitor indoor farms at the lab with Augmented Reality. Currently working on a coordinate-specific metric display for 3D plant models to monitor plant health and identify signs of diseases/deficiencies`,
+    technologies: [ "React Native", "Firebase", "Unity", "C#", "Javascript"],
   },
   {
-    year: "2024 - Present",
-    role: "Software Developer Intern",
+    year: "July 2024 - Present",
+    role: "Undergraduate Research Aide",
     company: "University at Buffalo",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
-    technologies: ["Javascript", "React Native", "Unity", "C#"],
+    description: `Maintaining Wireless Topology Tool, a Flask web app for topology simulation to integrate real geographic data for wireless network research. Integrate a dynamic display feature for NS3 simulation results by transforming large JSON topology data.`,
+    technologies: ["Flask", "MySQL", "SQLAlchemy", "JavaScript", "Python"],
   },
   {
-    year: "2024 - Present",
-    role: "Software Developer Intern",
-    company: "University at Buffalo",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
-    technologies: ["Javascript", "React Native", "Unity", "C#"],
+    year: "Jan 2020 - Jun 2022",
+    role: "Founder & Coding Instructor",
+    company: "Brown Women for STEM",
+    description: `Provided 30 underprivileged students with one-on-one lessons in Computer Science and Mathematics once a week. Connected 7 women with full-ride college scholarships totaling $151,000 by mentoring students through their
+application processes, holding monthly meet-ups to facilitate access to resources and opportunities.`,
+    technologies: ["HTML", "CSS", "Javascript", "Python"],
   },
   {
-    year: "2024 - Present",
-    role: "Software Developer Intern",
-    company: "University at Buffalo",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
-    technologies: ["Javascript", "React Native", "Unity", "C#"],
-  },
+    year: "June 2020 - Dec 2022",
+    role: "Technical Lead",
+    company: "Amino Apps",
+    description: `Supervised a team of 20 curators, overseeing an online community of 2 million+ members under Narvii, Inc. Settled 20+ support tickets daily using internal dev tools and Jira to resolve technical flags in public servers.`,
+    technologies: [],
+  }
 ];
 
 export const PROJECTS = [
