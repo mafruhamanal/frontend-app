0cfedb2207c4ee2809a46f781703de72985bbb09
mafruhamanal <mafruhamanal1@gmail.com>
2024-07-25 03:14:31 -0400

    change main pic

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/profilepic 1.png";
+import profilePic from "../assets/profilepic.png";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
